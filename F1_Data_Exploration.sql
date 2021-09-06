d6c9af04bf4820554b9c97d8ff7661d04b921c20
ritgupta31 <73716385+ritgupta31@users.noreply.github.com>
2021-09-06 19:53:53 -0400

    Update F1_Data_Exploration.sql

--- F1_Data_Exploration.sql
+++ F1_Data_Exploration.sql
@@ -23,71 +23,49 @@ ORDER BY NumberOfRaces DESC;
 -- Creating a View to save time in writing complex queries 
 
 CREATE VIEW RaceResults AS
-(SELECT
-	races.name, races.year, drivers.driverId, CONCAT(drivers.forename, ' ', drivers.surname) AS driver, results.grid, results.points, results.position, results.rank
-FROM
-	results
-INNER JOIN
-	drivers
-ON 
-	results.driverId = drivers.driverId
-INNER JOIN 
-	races
-ON 
-	results.raceId = races.raceId
-ORDER BY 
-	races.year, races.name);
+(SELECT races.name, races.year, drivers.driverId, CONCAT(drivers.forename, ' ', drivers.surname) AS driver, 
+ results.grid, results.points, results.position, results.rank
+FROM results
+INNER JOIN drivers
+ON results.driverId = drivers.driverId
+INNER JOIN races
+ON results.raceId = races.raceId
+ORDER BY races.year, races.name);
 
-
-    
+  
 -- Total points won by each driver and respective final rank in each championship
 
-SELECT
-	year, driver, sum(convert(points, signed)) AS TotalPoints, RANK() OVER (PARTITION BY year ORDER BY sum(convert(points, signed)) DESC) AS ChampionshipRank
-FROM
-	raceresults
-GROUP BY
-	driver, year
-ORDER BY
-	year DESC;
+SELECT year, driver, sum(convert(points, signed)) AS TotalPoints, 
+RANK() OVER (PARTITION BY year ORDER BY sum(convert(points, signed)) DESC) AS ChampionshipRank
+FROM raceresults
+GROUP BY driver, year
+ORDER BY year DESC;
 
 
 -- Driver with the most number of wins till date
 
-SELECT 
-	driver, count(position) AS Wins
-FROM 
-	raceresults
-WHERE
-	position = 1
-GROUP BY
-	driver
-ORDER BY 
-	Wins DESC;
+SELECT driver, count(position) AS Wins
+FROM raceresults
+WHERE position = 1
+GROUP BY driver
+ORDER BY Wins DESC;
 
 
 -- Drivers and number of their championship wins
     
 
-SELECT 
-	a.driver, count(a.ChampionshipRank) AS ChampionshipWins
+SELECT a.driver, count(a.ChampionshipRank) AS ChampionshipWins
 FROM
-	(
-(SELECT
-	year, driver, sum(convert(points, signed)) AS TotalPoints, RANK() OVER (PARTITION BY year ORDER BY sum(convert(points, signed)) DESC) AS ChampionshipRank
-FROM
-	raceresults
-GROUP BY
-	driver, year
-ORDER BY
-	year DESC, TotalPoints DESC)
-    ) AS a
-WHERE
-	ChampionshipRank = 1
-GROUP BY
-	a.driver
-ORDER BY
-	ChampionshipWins DESC;
+(
+	SELECT year, driver, sum(convert(points, signed)) AS TotalPoints, 
+	RANK() OVER (PARTITION BY year ORDER BY sum(convert(points, signed)) DESC) AS ChampionshipRank
+	FROM raceresults
+	GROUP BY driver, year
+	ORDER BY year DESC, TotalPoints DESC
+) AS a
+WHERE ChampionshipRank = 1
+GROUP BY a.driver
+ORDER BY ChampionshipWins DESC;
     
 
 
@@ -95,26 +73,18 @@ ORDER BY
 
 SELECT a.circuit, a.location, a.country, a.driver, count(a.driver) AS TimesWon
 FROM
-(SELECT
-	races.year, races.name, circuits.name AS circuit , circuits.location, circuits.country, CONCAT(drivers.forename, ' ', drivers.surname) AS driver, results.points, results.position
-FROM
-	results
-INNER JOIN
-	drivers
-ON 
-	results.driverId = drivers.driverId
-INNER JOIN 
-	races
-ON 
-	results.raceId = races.raceId
-INNER JOIN
-	circuits
-ON
-	races.circuitId = circuits.circuitId
-WHERE
-	results.position = 1
-ORDER BY 
-	races.year DESC, races.name) AS a
+(	SELECT races.year, races.name, circuits.name AS circuit , circuits.location, circuits.country, 
+	 CONCAT(drivers.forename, ' ', drivers.surname) AS driver, results.points, results.position
+	FROM results
+	INNER JOIN drivers
+	ON results.driverId = drivers.driverId
+	INNER JOIN races
+	ON results.raceId = races.raceId
+	INNER JOIN circuits
+	ON races.circuitId = circuits.circuitId
+	WHERE results.position = 1
+	ORDER BY races.year DESC, races.name
+) AS a
 GROUP BY a.circuit, a.driver, a.location, a.country
 ORDER BY a.circuit, TimesWon DESC;
 
@@ -125,36 +95,30 @@ SELECT b.circuit, b.location, b.country, b.driver, b.TimesWon, c.TimesRaced, (b.
 FROM
 	(SELECT a.circuit, a.location, a.country, a.driver, count(a.driver) AS TimesWon
 	FROM
-		(SELECT circuits.name AS circuit, circuits.location, circuits.country, CONCAT(drivers.forename, ' ', drivers.surname) AS driver, results.position
+		(SELECT circuits.name AS circuit, circuits.location, circuits.country, 
+		 CONCAT(drivers.forename, ' ', drivers.surname) AS driver, results.position
 		FROM results
-		INNER JOIN 
-			drivers
+		INNER JOIN drivers
 		ON results.driverId = drivers.driverId
-		 JOIN 
-			races
+		 JOIN races
 		ON results.raceId = races.raceId
-		INNER JOIN
-			circuits
+		INNER JOIN circuits
 		ON races.circuitId = circuits.circuitId
-		WHERE
-			results.position = 1) AS a
+		WHERE results.position = 1) AS a
 	GROUP BY a.circuit, a.driver, a.location, a.country
 	ORDER BY a.country, TimesWon DESC
     ) AS b
     INNER JOIN
-		(SELECT circuits.name AS circuit , circuits.location, CONCAT(drivers.forename, ' ', drivers.surname) AS driver, count(drivers.driverId) AS TimesRaced
+		(SELECT circuits.name AS circuit , circuits.location, 
+		 CONCAT(drivers.forename, ' ', drivers.surname) AS driver, count(drivers.driverId) AS TimesRaced
 		FROM results
-		INNER JOIN
-			drivers
+		INNER JOIN drivers
 		ON results.driverId = drivers.driverId
-		INNER JOIN 
-			races
+		INNER JOIN races
 		ON results.raceId = races.raceId
-		INNER JOIN
-			circuits
+		INNER JOIN circuits
 		ON races.circuitId = circuits.circuitId
-		GROUP BY 
-			circuits.name, circuits.location, CONCAT(drivers.forename, ' ', drivers.surname)
+		GROUP BY circuits.name, circuits.location, CONCAT(drivers.forename, ' ', drivers.surname)
         ) AS c ON b.circuit = c.circuit and b.driver = c.driver 
 ORDER BY PercentWon DESC;
     
@@ -168,19 +132,16 @@ CREATE TEMPORARY TABLE WinsAtEachCircuit
 INSERT INTO WinsAtEachCircuit (circuit, location, country, driver, TimesWon)
 SELECT a.circuit, a.location, a.country, a.driver, count(a.driver) AS TimesWon
 	FROM
-		(SELECT circuits.name AS circuit, circuits.location, circuits.country, CONCAT(drivers.forename, ' ', drivers.surname) AS driver, results.position
+		(SELECT circuits.name AS circuit, circuits.location, circuits.country, 
+		 CONCAT(drivers.forename, ' ', drivers.surname) AS driver, results.position
 		FROM results
-		INNER JOIN 
-			drivers
+		INNER JOIN drivers
 		ON results.driverId = drivers.driverId
-		 JOIN 
-			races
+		 JOIN races
 		ON results.raceId = races.raceId
-		INNER JOIN
-			circuits
+		INNER JOIN circuits
 		ON races.circuitId = circuits.circuitId
-		WHERE
-			results.position = 1) AS a
+		WHERE results.position = 1) AS a
 	GROUP BY a.circuit, a.driver, a.location, a.country
 	ORDER BY TimesWon DESC;
 
@@ -190,23 +151,17 @@ CREATE TEMPORARY TABLE DriversAtEachCircuit
 (circuit text, location text, country text, driver text, TimesRaced int);
     
 INSERT INTO DriversAtEachCircuit(circuit, location, country, driver, TimesRaced)
-SELECT
-	circuits.name AS circuit , circuits.location, circuits.country, CONCAT(drivers.forename, ' ', drivers.surname) AS driver, count(drivers.driverId) AS TimesRaced
-FROM
-	results
-INNER JOIN
-	drivers
+SELECT circuits.name AS circuit , circuits.location, circuits.country, 
+CONCAT(drivers.forename, ' ', drivers.surname) AS driver, count(drivers.driverId) AS TimesRaced
+FROM results
+INNER JOIN drivers
 ON results.driverId = drivers.driverId
-INNER JOIN 
-	races
+INNER JOIN races
 ON results.raceId = races.raceId
-INNER JOIN
-	circuits
+INNER JOIN circuits
 ON races.circuitId = circuits.circuitId
-GROUP BY 
-	circuits.name, circuits.location, circuits.country, CONCAT(drivers.forename, ' ', drivers.surname)
-ORDER BY 
-	TimesRaced DESC;
+GROUP BY circuits.name, circuits.location, circuits.country, CONCAT(drivers.forename, ' ', drivers.surname)
+ORDER BY TimesRaced DESC;
     
 
 SELECT a.circuit, a.location, a.country, a.driver, (b.TimesWon/a.TimesRaced)*100 AS PercentWon
@@ -220,60 +175,39 @@ ORDER BY PercentWon DESC;
 
 SELECT a.name, a.circuit, a.location, min(a.fastestLapTime) AS FastestLapTimeEver
 FROM
-(SELECT
-	races.year, races.name, circuits.name AS circuit , circuits.location, circuits.country, results.fastestLapTime
-FROM
-	results
-INNER JOIN
-	races
-ON 
-	results.raceId = races.raceId
-INNER JOIN
-	circuits
-ON
-	races.circuitId = circuits.circuitId
-WHERE
-	results.fastestLapTime NOT LIKE '%N%'
-ORDER BY 
-	races.year DESC, races.name) AS a
-GROUP BY
-	a.name, a.circuit, a.location
-ORDER BY 
-	a.name, a.location;
-
+(SELECT races.year, races.name, circuits.name AS circuit , circuits.location, circuits.country, results.fastestLapTime
+FROM results
+INNER JOIN races
+ON results.raceId = races.raceId
+INNER JOIN circuits
+ON races.circuitId = circuits.circuitId
+WHERE results.fastestLapTime NOT LIKE '%N%'
+ORDER BY races.year DESC, races.name) AS a
+GROUP BY a.name, a.circuit, a.location
+ORDER BY a.name, a.location;
 
 
 -- Constructors Champioship
 
-SELECT 
-	races.year, races.date, races.name, constructors.name, constructor_results.points
-FROM 
-	races
-INNER JOIN
-	constructor_results
-ON
-	races.raceId = constructor_results.raceId
-INNER JOIN
-	constructors
-ON
-	constructor_results.constructorId = constructors.constructorId
-ORDER BY
-	races.date DESC, points DESC;
+SELECT races.year, races.date, races.name, constructors.name, constructor_results.points
+FROM races
+INNER JOIN constructor_results
+ON races.raceId = constructor_results.raceId
+INNER JOIN constructors
+ON constructor_results.constructorId = constructors.constructorId
+ORDER BY races.date DESC, points DESC;
 
 
 -- Team with most number of championship wins (Using CTE)
 
 WITH ConstructorWins (year, name, points, position) AS
 (
-SELECT 
-	races.year AS year, constructors.name, sum(constructor_results.points) AS points, RANK() OVER (PARTITION BY year ORDER BY sum(constructor_results.points) DESC)
-FROM 
-	races
-INNER JOIN
-	constructor_results
+SELECT races.year AS year, constructors.name, sum(constructor_results.points) AS points, 
+	RANK() OVER (PARTITION BY year ORDER BY sum(constructor_results.points) DESC)
+FROM races
+INNER JOIN constructor_results
 ON races.raceId = constructor_results.raceId
-INNER JOIN
-	constructors
+INNER JOIN constructors
 ON constructor_results.constructorId = constructors.constructorId
 GROUP BY year, constructors.name
 ORDER BY year DESC, points DESC
